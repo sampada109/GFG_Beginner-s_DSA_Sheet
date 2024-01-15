ebdacba4d063ef7c0e6bf96b41d4970d6245672b
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-01-15 21:59:25 +0530

    Create Grinding Geek.py

---
+++ Grinding Geek/Grinding Geek.py
@@ -0,0 +1,17 @@
+import math
+
+def f(i, total, cost, dp):
+            if i == n or total <= 0:
+                return 0
+            if (i,total) in dp:
+                return dp[(i, total)]
+            dp[(i,total)] = max(1 + f(i + 1, math.floor(total - (10 / 100 * cost[i])), cost, dp) if cost[i] <= total else 0, f(i + 1, total, cost, dp))
+            return dp[(i,total)]
+
+class Solution:
+    def max_courses(self, n : int, total : int, cost : List[int]) -> int:
+        # code here
+        
+        dp = {}
+        
+        return f(0, total, cost, dp)
